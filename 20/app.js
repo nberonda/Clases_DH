7664a0998688d92d62a912b51ac258bdac904400
nberonda <nicolasbc0@gmail.com>
2023-03-21 16:34:06 -0300

    Clase 20 parte 1

--- 20/app.js
+++ 20/app.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 const app = express();
 
+app.get ('/', function(req,res) {
+    res.sendFile (path.join (__dirname, 'formularios.html'))
+})
+
 app.listen (3000, () => {
     console.log ('funka http://localhost:3000');
 });
