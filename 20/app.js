9a7eba763f678239f18937bf916636598b40265a
nberonda <nicolasbc0@gmail.com>
2023-03-22 20:26:26 -0300

    clase 20

--- 20/app.js
+++ 20/app.js
@@ -4,7 +4,15 @@ const path = require('path');
 const app = express();
 
 app.get ('/', function(req,res) {
-    res.sendFile (path.join (__dirname, 'formularios.html'))
+    res.sendFile (path.join (__dirname, 'formularios.html'));
+});
+
+app.get ('/2', function(req,res) {
+    res.sendFile (path.join (__dirname, 'radio_button_checks.html'));
+});
+
+app.get ('/3', (req,res) => {
+    res.sendFile (path.resolve (__dirname, 'formularios_avanzados.html'))
 })
 
 app.listen (3000, () => {
