1db3b30eca5b08aaeaefc0121a2b0dc389df6863
nberonda <nicolasbc0@gmail.com>
2023-03-31 22:14:57 -0300

    22

---
+++ 22/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get ('/', function(req,res) {
+    res.sendFile (path.join (__dirname, 'animaciones.html'));
+});
+
+app.get ('/2', function(req,res) {
+    res.sendFile (path.join (__dirname, 'radio_button_checks.html'));
+});
+
+app.get ('/3', (req,res) => {
+    res.sendFile (path.resolve (__dirname, 'formularios_avanzados.html'))
+})
+
+app.listen (3000, () => {
+    console.log ('funka http://localhost:3000');
+});
